825a51aea412b595e69ddc3284ace520375dbc89
Jalaj <jalajdorai100@gmail.com>
2020-04-02 16:36:53 +0530

    Updates

---
+++ precache-manifest.5996130f78bbba1629d21ce337871a04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e644795f0d350f582abf3f59cc998e",
+    "url": "/ecommerce-cart/index.html"
+  },
+  {
+    "revision": "ef66359daf0f7452d362",
+    "url": "/ecommerce-cart/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "92100191066323541fbc",
+    "url": "/ecommerce-cart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ef66359daf0f7452d362",
+    "url": "/ecommerce-cart/static/js/2.0107cb0f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ecommerce-cart/static/js/2.0107cb0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92100191066323541fbc",
+    "url": "/ecommerce-cart/static/js/main.4ac64ff8.chunk.js"
+  },
+  {
+    "revision": "e6c7f1747e64af490f56",
+    "url": "/ecommerce-cart/static/js/runtime-main.1fe8b127.js"
+  }
+]);
